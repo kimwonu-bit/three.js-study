12911697378ebf16596f4836e09eeed5c6ee8c6e
김원우 <25_13@bssm.hs.kr>
2025-07-30 18:26:12 +0900

    first commit

---
+++ basic_vite/src/main.js
@@ -0,0 +1,9 @@
+import * as THREE from 'three';
+
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+const canvas = document.querySelector('#three-canvas');
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerWidth);
