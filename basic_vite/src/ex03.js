5c6456a9130b8bd6ae76b0ea24532e40a8bcf402
김원우 <25_13@bssm.hs.kr>
2025-08-04 12:31:35 +0900

    투명도

---
+++ basic_vite/src/ex03.js
@@ -0,0 +1,56 @@
+import * as THREE from "three";
+import { Sequence } from "three/examples/jsm/libs/tween.module.js";
+import { reduceVertices } from "three/examples/jsm/utils/SceneUtils.js";
+
+export default function example() {
+    //렌더러
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,//계단 현상 제거
+    alpha: true//이 코드를 써야 setClearAlpha() 함수 사용 가능
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+  renderer.setClearAlpha(0.5)//인자를 0 ~ 1 사이의 수로 받아 불투명도를 정할 수 있음
+  renderer.setClearColor('green');
+
+  //씬
+  const scene = new THREE.Scene();
+//   scene.background = new.THREE.Color('blue');//씬 자체에 칠하는 것.오류 이유 모름...
+
+
+
+  //카메라
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  camera.position.set(1, 2, 5);
+  scene.add(camera);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: "red" });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  // ✅ 애니메이션 루프 (계속 그리기)
+  function draw() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(draw);
+  }
+  draw(); // 첫 실행
+
+  // ✅ 리사이즈 대응
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  window.addEventListener("resize", setSize);
+}
